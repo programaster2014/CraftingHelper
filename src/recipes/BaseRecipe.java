808b2ecc5546321165ecc9bf94b8866cccbaf099
programaster2014 <robbrunner2014@gmail.com>
2019-03-25 16:36:38 -0400

    added price calculation

--- src/recipes/BaseRecipe.java
+++ src/recipes/BaseRecipe.java
@@ -1,72 +1,32 @@
 package recipes;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-
 import javax.swing.JProgressBar;
 
 import gwClasses.Requirement;
 import network.DataRetriever;
-import thread.TreeCaller;
 import tree.GWTree;
 
 public class BaseRecipe {
 	public String name;
+	public int id;
 	private DataRetriever ret;
-	public HashMap<String, HashMap<Integer, Requirement>> recipes;
-	public HashMap<Integer, Integer> order;
-	public HashMap<Integer, String> recipeNames;
+	public HashMap<Integer, Requirement> recipe;
 	
-	public BaseRecipe(DataRetriever ret){
+	public BaseRecipe(DataRetriever ret, int id){
 		this.ret = ret;
-		
-		recipes = new HashMap<>();
-		order = new HashMap<>();
-		recipeNames = new HashMap<>();
+		this.id = id;
+		recipe = new HashMap<>();
 	}
 	
 	public void generateRecipe() {
-		for(Integer count = 1; count <= order.size(); count++) {
-			if(!(order.get(count) == -999)) {
-				GWTree tree = new GWTree(order.get(count), ret);
-				tree.build();
-				recipeNames.put(count, ret.getItemNameById(order.get(count)));
-				recipes.put(tree.getItemName(), tree.baseElements);
-			}
-		}
+		GWTree tree = new GWTree(id, ret);
+		tree.build();
 	}
 	
-	public void generateRecipe(JProgressBar progress) {
-		int step = 80 / order.size();
-		ExecutorService executor = Executors.newFixedThreadPool(10);
-		ArrayList<TreeCaller> gwTreeList = new ArrayList<>();
-		
-		for(Integer counter = 1; counter <= order.size(); counter++) {
-			if(!(order.get(counter) == -999)) {
-				gwTreeList.add(new TreeCaller(counter, order.get(counter), ret));				
-			}
-		}
-		
-		try {
-			List<Future<GWTree>> futures = executor.invokeAll(gwTreeList);
-			
-			for(Future<GWTree> future : futures) {
-				GWTree tree = future.get();
-				recipeNames.put(tree.Count, tree.getItemName());
-				recipes.put(tree.getItemName(), tree.baseElements);
-				progress.setValue(progress.getValue() + step);
-			}
-			
-		} catch (InterruptedException | ExecutionException e) {
-			e.printStackTrace();
-		}
-		
-		executor.shutdown();
-
+	public void generateRecipe(JProgressBar progress) {	
+		GWTree tree = new GWTree(id, ret);
+		tree.build();
+		this.recipe = tree.baseElements;
 	}
 }
